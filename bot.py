bd6bbc2851d4208b8ed732e6862c1441d989008d
Harsh <65716674+Harsh-br0@users.noreply.github.com>
2022-02-24 19:43:16 +0530

    fix clear cache , testing stream end handler

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@
 import os
 import shutil
 import time
+from config import Config
+from itertools import chain
 from datetime import datetime
 from collections import defaultdict
-from config import Config
 from pyrogram import filters, Client, idle
 from pytgcalls import PyTgCalls
 from pytgcalls.types import AudioPiped
@@ -13,7 +14,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 VOICE_CHATS = dict()
 DEFAULT_DOWNLOAD_DIR = os.path.join(os.getcwd(), 'downloads/')
-PLAYING = defaultdict(lambda: "")
+PLAYING = defaultdict(tuple)
 QUEUE = defaultdict(list)
 FILES = defaultdict(list)
 START_TIME = time.time()
@@ -57,65 +58,74 @@ def format_time(seconds):
 
 
 def get_scheduled_text(chat, title, link):
-	s = "Scheduled [{title}]({link}) on #{position} position"
-	return s.format(title=title, link=link, position=len(QUEUE[chat])+1)
+    s = "Scheduled [{title}]({link}) on #{position} position"
+    return s.format(title=title, link=link, position=len(QUEUE[chat])+1)
 
 
-def get_first_song(title, link):
-	return f"[{title}]({link})"
+def get_formatted_link(title, link):
+    return f"[{title}]({link})"
+
+def clear_cache(all = False):
+    if all:
+        return shutil.rmtree(DEFAULT_DOWNLOAD_DIR, ignore_errors=True)
+    _playlist = map(lambda x:x[1], PLAYING.values())
+    for i in chain.from_iterable(FILES.values()):
+        if i not in _playlist:
+            os.remove(i)
 
 async def tg_down(message):
-	audio = message.audio
-	my_message = await message.reply('Downloading...')
-	audio_original = await message.download(DEFAULT_DOWNLOAD_DIR)
+    audio = message.audio
+    my_message = await message.reply('Downloading...')
+    audio_original = await message.download(DEFAULT_DOWNLOAD_DIR)
 
-	await my_message.edit("Passing it to VC...")
-	await vc.change_stream(
-		message.chat.id,
-		AudioPiped(audio_original)
-	)
-	FILES[message.chat.id].append(audio_original)
-	await my_message.edit(f"Playing **[{audio.title}]({message.link})**")
-
-
-async def _skip(c, __):
-	await handle_queue(c)
+    await my_message.edit("Passing it to VC...")
+    await vc.change_stream(
+        message.chat.id,
+        AudioPiped(audio_original)
+    )
+    FILES[message.chat.id].append(audio_original)
+    await my_message.edit(f"Playing **[{audio.title}]({message.link})**")
+    return audio_original
 
 
 async def handle_queue(chat_id, clear = False):
-	
-	await vc.change_stream(
-		chat_id,
-		AudioPiped(
-			'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
-		)
+    
+    await vc.change_stream(
+        chat_id,
+        AudioPiped(
+            'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
+        )
     )
 
-	if clear:
-		QUEUE[chat_id].clear()
+    if clear:
+        QUEUE[chat_id].clear()
 
-	if not QUEUE[chat_id]:
-		PLAYING[chat_id] = ""
-		return
+    if not QUEUE[chat_id]:
+        PLAYING[chat_id] = tuple()
+        return
 
-	shutil.rmtree(DEFAULT_DOWNLOAD_DIR, ignore_errors=True) # here needs to delete files 
-	msg = QUEUE[chat_id].pop(0)
-	try:
-		PLAYING[chat_id] = get_first_song(msg.audio.title, msg.link)
-		await tg_down(msg)
-	except Exception as err:
-		PLAYING[chat_id] = ""
-		out = f"**ERROR:** `{str(err)}`"
-		if QUEUE[chat_id]:
-			out += "\n\n`Playing next Song.`"
-		await app.send_message(
-			chat_id,
-			out,
-			disable_web_page_preview=True
-	    )
-		await handle_queue(chat_id)
-	finally:
-		shutil.rmtree(DEFAULT_DOWNLOAD_DIR, ignore_errors=True) # here needs to delete files 
+    clear_cache()
+    msg = QUEUE[chat_id].pop(0)
+    try:
+        PLAYING[chat_id] = get_formatted_link(msg.audio.title, msg.link), await tg_down(msg)
+    except Exception as err:
+        PLAYING[chat_id] = tuple()
+        out = f"**ERROR:** `{str(err)}`"
+        if QUEUE[chat_id]:
+            out += "\n\n`Playing next Song.`"
+        await app.send_message(
+            chat_id,
+            out,
+            disable_web_page_preview=True
+        )
+        await handle_queue(chat_id)
+    finally:
+        clear_cache() 
+
+
+@vc.on_stream_end()
+async def _skip(_, u):
+    await app.send_message(u.chat_id, str(u))
 
 
 @app.on_message(filters.command('ping') & self_or_contact_filter)
@@ -125,7 +135,7 @@ async def ping(_, message):
     end = datetime.now()
     m_s = (end - start).microseconds / 1000
     uptime = format_time(time.time() - START_TIME)
-    await rape.edit(f'**Pong!**\n> `{m_s} ms`\n\n**Uptime**\n> `{uptime}`')
+    await rape.edit(f'**Pong!**\n> `{m_s} ms`\n\n**NodeJS Core Ping**\n> `{vc.ping} ms`\n\n**Uptime**\n> `{uptime}`')
 
 
 @app.on_message(((autoqueue_filter & filters.audio) | filters.command('play')) & self_or_contact_filter)
@@ -137,9 +147,9 @@ async def play_track(_, message):
         try:
             await vc.join_group_call(
                 message.chat.id,
-				AudioPiped(
-					'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
-				)
+                AudioPiped(
+                    'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
+                )
             )
         except NoActiveGroupCall:
             await message.reply('First start a VC in this group rotor')
@@ -154,102 +164,103 @@ async def play_track(_, message):
 
 @app.on_message(filters.command('stop') & self_or_contact_filter)
 async def stop_playing(__, message):
-	await handle_queue(message.chat.id, True)
-	await vc.change_stream(
-		message.chat.id,
-		AudioPiped(
-			'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
-		)
+    await handle_queue(message.chat.id, True)
+    await vc.change_stream(
+        message.chat.id,
+        AudioPiped(
+            'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
+        )
     )
-	for i in FILES[message.chat.id]:
-		os.remove(i)
-		FILES[message.chat.id].clear()
-	await message.reply('Stopped')
+    clear_cache(True)
+    await message.reply('Stopped')
 
 
 @app.on_message(filters.command('joinvc') & self_or_contact_filter)
 async def join_voice_chat(_, message):
-	if message.chat.id in list(map(lambda x: x.chat_id, vc.calls)):
-		await message.reply('Already in')
-		return
-	try:
-		await vc.join_group_call(
-			message.chat.id,
-			AudioPiped(
-				'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
-			)
+    if message.chat.id in list(map(lambda x: x.chat_id, vc.calls)):
+        await message.reply('Already in')
+        return
+    try:
+        await vc.join_group_call(
+            message.chat.id,
+            AudioPiped(
+                'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
+            )
         )
-	except NoActiveGroupCall:
-		await message.reply('First start a VC in this group rotor')
-		return
-	except Exception as e:
-		await message.reply('Error Now rape me!')
-		print(e)
-		return
-	await message.reply('Joined ')
+    except NoActiveGroupCall:
+        await message.reply('First start a VC in this group rotor')
+        return
+    except Exception as e:
+        await message.reply('Error Now rape me!')
+        print(e)
+        return
+    await message.reply('Joined ')
 
 
 @app.on_message(filters.command('leavevc') & self_or_contact_filter)
 async def leave_voice_chat(_, message):
-	chat_id = message.chat.id
-	await handle_queue(chat_id, True)
-	await vc.leave_group_call(
-		message.chat.id,
-	)
-	VOICE_CHATS.pop(chat_id, None)
-	await message.reply('Left')
+    chat_id = message.chat.id
+    await handle_queue(chat_id, True)
+    await vc.leave_group_call(
+        message.chat.id,
+    )
+    VOICE_CHATS.pop(chat_id, None)
+    await message.reply('Left')
 
 
 @app.on_message(filters.command('leave_group') & self_or_contact_filter)
 async def leave_group(_, message):
-	if len(message.command) != 2:
-		await message.reply_text("/leave_group `GroupID`")
-		return
-	try:
-		chat_id = message.text.split(None, 1)[1]
-		await app.leave_chat(chat_id)
-		await message.reply_text("`Successfully Left That Group`")
-	except Exception as e:
-		await message.reply_text(str(e))
-		print(str(e))
+    if len(message.command) != 2:
+        await message.reply_text("/leave_group `GroupID`")
+        return
+    try:
+        chat_id = message.text.split(None, 1)[1]
+        await app.leave_chat(chat_id)
+        await message.reply_text("`Successfully Left That Group`")
+    except Exception as e:
+        await message.reply_text(str(e))
+        print(str(e))
+
 
 @app.on_message(filters.command('join_group') & self_or_contact_filter)
 async def join_group(_, message):
-	if len(message.command) != 2:
-		await message.reply_text("/join_group `GroupID`")
-		return
-	try:
-		chat_id = message.text.split(None, 1)[1]
-		await app.join_chat(chat_id)
-		await message.reply_text("`Successfully Joined That Group`")
-	except Exception as e:
-		await message.reply_text(str(e))
-		print(str(e))
+    if len(message.command) != 2:
+        await message.reply_text("/join_group `GroupID`")
+        return
+    try:
+        chat_id = message.text.split(None, 1)[1]
+        await app.join_chat(chat_id)
+        await message.reply_text("`Successfully Joined That Group`")
+    except Exception as e:
+        await message.reply_text(str(e))
+        print(str(e))
 
-@app.on_message(filters.command('clear_cache') & self_or_contact_filter)
-async def clear_cache(_, message):
-	for i in FILES[message.chat.id]:
-		os.remove(i)
-		FILES[message.chat.id].clear()
-	await message.reply_text("Cleared all Downloaded Files")
+
+@app.on_message(filters.command('cacheclear') & self_or_contact_filter)
+async def _clear_cache(_, message):
+    clear_cache()
+    await message.reply_text("Cleared all Downloaded Files")
+
 
 @app.on_message(filters.command('queue') & self_or_contact_filter)
 async def show_queue(_, message):
-	queue = QUEUE[message.chat.id]
-	_no = (len(queue) + 1) if PLAYING[message.chat.id] else len(queue)
-	if not _no:
-		return await message.reply("Queue is empty.")
-	msg = (f'**__{_no} Song{"s" if _no > 1 else ""} in queue:__**\n\n'
-			f"▶ {PLAYING[message.chat.id]}\n")
-	for m in queue:
-		msg += f"● [{m.audio.title}]({m.link})\n"
-	await message.reply(msg.strip(), disable_web_page_preview=True)
+    queue = QUEUE[message.chat.id]
+    _no = (len(queue) + 1) if PLAYING[message.chat.id] else len(queue)
+    if not _no:
+        return await message.reply("Queue is empty.")
+    msg = (f'**__{_no} Song{"s" if _no > 1 else ""} in queue:__**\n\n'
+            f"▶ {PLAYING[message.chat.id][0]}\n")
+    for m in queue:
+        msg += f"● {get_formatted_link(m.audio.title, m.link)}\n"
+    await message.reply(msg.strip(), disable_web_page_preview=True)
+
 
 @app.on_message(filters.command('skip') & self_or_contact_filter)
 async def skip_song(_, message):
-	if message.chat.id in list(map(lambda x: x.chat_id, vc.calls)):
-		await handle_queue(message.chat.id)
-		await message.reply("Skipped")
+    if message.chat.id in list(map(lambda x: x.chat_id, vc.calls)):
+        await handle_queue(message.chat.id)
+        await message.reply("Skipped")
+
 
 @app.on_message(filters.command('auto') & self_or_contact_filter)
 async def auto_queue(_, message):
